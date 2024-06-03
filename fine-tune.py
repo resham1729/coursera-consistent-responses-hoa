e78167bc1101f57e50814228695fc4988ad2796c
Resham Sivnarain <email@example.com>
2024-06-03 16:51:19 +0200

    Split the fine-tune script and prompt-test scripts

---
+++ fine-tune.py
@@ -0,0 +1,34 @@
+import jsonlines 
+from openai import OpenAI
+
+# Set up your OpenAI API key
+client = OpenAI(
+      api_key = 'sk-proj-8YFNRzEPyiFaxHECpVfLT3BlbkFJn8HeGigtuvv2LfKrbxQP'  # Replace 'your_openai_api_key' with your actual API key
+)
+
+# Load your JSONL dataset
+dataset_path = './Data/dataset1.jsonl'  # Replace 'your_dataset.jsonl' with your actual dataset path
+data = []
+with jsonlines.open(dataset_path) as reader:
+    for line in reader:
+        data.append(line)
+
+# Prepare your data in the format required by OpenAI
+training_data = []
+for item in data:
+    prompt = item['messages'][1]['content']
+    answer = item['messages'][2]['content']
+    training_data.append({"prompt": prompt, "completion": answer})
+
+
+client.files.create(
+  file=open("./Data/dataset1.jsonl", "rb"),
+  purpose="fine-tune"
+)
+
+# Create a fine-tuned model
+client.fine_tuning.jobs.create(
+  training_file=client.files.retrieve("file-RVD1Adlzyx9cuspClUOk6uVR"), 
+  model="gpt-3.5-turbo"
+)
+
---
+++ prompt-test.py
@@ -0,0 +1,39 @@
+
+import jsonlines 
+from openai import OpenAI
+
+## Set up your OpenAI API key
+client = OpenAI(
+      api_key = 'sk-proj-8YFNRzEPyiFaxHECpVfLT3BlbkFJn8HeGigtuvv2LfKrbxQP'  # Replace 'your_openai_api_key' with your actual API key
+)
+
+# Define the prompt and model parameters
+
+# prompt = "Can you recommend some educational toys for 5-year-olds?"
+prompt = "Do you sell cars?"
+# model = "gpt-3.5-turbo"
+model = "ft:gpt-3.5-turbo-0125:personal::9UsZrg0l"  # Replace 'your_model' with your actual Model name
+stop = ["\n", "Assistant:"]  # Specify stopping sequences
+
+# Define a function to test the prompt with different temperature settings
+def test_prompt(prompt, model, temperature):
+    response = client.chat.completions.create(
+        model=model,
+        messages=[
+            {"role": "system", "content": "Mellow is a friendly and helpful chatbot for retails clothing store for men and women."},
+            {"role": "user", "content": prompt}
+        ],
+        temperature=temperature,
+        max_tokens=100,
+        stop=stop
+    )
+    return response.choices[0].message.content
+
+
+# Test the prompt with different temperature settings
+temperature_settings = [0.1, 0.5, 0.9]
+
+for temp in temperature_settings:
+    print(f"\nTesting with temperature={temp}:")
+    result = test_prompt(prompt, model, temp)
+    print(result)
